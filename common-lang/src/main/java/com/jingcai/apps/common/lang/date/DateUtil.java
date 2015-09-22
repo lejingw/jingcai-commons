b1568f5ed07d84cfd1782f93db4f45e8c7685d37
wzh <123511583@qq.com>
2015-09-22 17:12:45 +0800

    JPush

--- common-lang/src/main/java/com/jingcai/apps/common/lang/date/DateUtil.java
+++ common-lang/src/main/java/com/jingcai/apps/common/lang/date/DateUtil.java
@@ -1,5 +1,7 @@
 package com.jingcai.apps.common.lang.date;
 
+import org.apache.commons.lang3.time.DateFormatUtils;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -7,10 +9,14 @@ import java.util.Date;
 /**
  * Created by lejing on 15/4/16.
  */
-public class DateUtil {
+public class DateUtil extends org.apache.commons.lang3.time.DateUtils{
     private static final SimpleDateFormat formatter10 = new SimpleDateFormat("yyyy-MM-dd");
     private static final SimpleDateFormat formatter20 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     private static final SimpleDateFormat date14 = new SimpleDateFormat("yyyyMMddHHmmss");
+    private static String[] parsePatterns = {
+            "yyyy-MM-dd", "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd HH:mm", "yyyy-MM",
+            "yyyy/MM/dd", "yyyy/MM/dd HH:mm:ss", "yyyy/MM/dd HH:mm", "yyyy/MM",
+            "yyyy.MM.dd", "yyyy.MM.dd HH:mm:ss", "yyyy.MM.dd HH:mm", "yyyy.MM"};
 
     public static String getNow10() {
         return formatter10.format(new Date());
@@ -71,6 +77,30 @@ public class DateUtil {
         else
             return false;
 
+    }
+
+    /**
+     * 得到日期字符串 默认格式（yyyy-MM-dd） pattern可以为："yyyy-MM-dd" "HH:mm:ss" "E"
+     */
+    public static String formatDate(Date date, Object... pattern) {
+        String formatDate = null;
+        if (pattern != null && pattern.length > 0) {
+            formatDate = DateFormatUtils.format(date, pattern[0].toString());
+        } else {
+            formatDate = DateFormatUtils.format(date, "yyyy-MM-dd");
+        }
+        return formatDate;
+    }
+
+    public static Date parseDate(Object str) {
+        if (str == null){
+            return null;
+        }
+        try {
+            return parseDate(str.toString(), parsePatterns);
+        } catch (ParseException e) {
+            return null;
+        }
     }
 
 }
