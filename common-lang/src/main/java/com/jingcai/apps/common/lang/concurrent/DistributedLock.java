9d694c5bff3847934f0901b56e16d5a9ef0d7f93
lejingw <lejingw@163.com>
2015-12-21 16:20:09 +0800

    1

--- common-lang/src/main/java/com/jingcai/apps/common/lang/concurrent/DistributedLock.java
+++ common-lang/src/main/java/com/jingcai/apps/common/lang/concurrent/DistributedLock.java
@@ -1,6 +1,10 @@
 package com.jingcai.apps.common.lang.concurrent;
 
-import java.io.IOException;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -8,16 +12,6 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
-
-import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooDefs;
-import org.apache.zookeeper.ZooKeeper;
-import org.apache.zookeeper.data.Stat;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * DistributedLock lock = null;
@@ -27,8 +21,7 @@ import org.slf4j.LoggerFactory;
  * 		//do something...
  * } catch (Exception e) {
  * 		e.printStackTrace();
- * }
- * finally {
+ * } finally {
  * 		if(lock != null)
  * 			lock.unlock();
  * }
@@ -36,38 +29,34 @@ import org.slf4j.LoggerFactory;
 public class DistributedLock implements Lock, Watcher {
 	private static final Logger logger = LoggerFactory.getLogger(DistributedLock.class);
 	private final String splitStr = "_lock_";
+	private final String root = "/locks";//根
 	private ZooKeeper zk;
-	private String root = "/locks";//根
 	private String lockName;//竞争资源的标志
 	private String waitNode;//等待前一个锁
 	private String myZnode;//当前锁
 	private CountDownLatch latch;//计数器
 	private int sessionTimeout = 30000;
-	private List<Exception> exception = new ArrayList<Exception>();
 
 	/**
-	 * 创建分布式锁,使用前请确认config配置的zookeeper服务可用
+	 * 创建分布式锁,使用前请确认address配置的zookeeper服务可用
 	 *
-	 * @param config   127.0.0.1:2181
+	 * @param address  127.0.0.1:2181
 	 * @param lockName 竞争资源标志,lockName中不能包含单词lock
 	 */
-	public DistributedLock(String config, String lockName) {
+	public DistributedLock(String address, String lockName) {
+		if (lockName.contains(splitStr)) {
+			throw new LockException("lockName can not contains " + splitStr);
+		}
 		this.lockName = lockName;
-		// 创建一个与服务器的连接
 		try {
-			zk = new ZooKeeper(config, sessionTimeout, this);
+			this.zk = new ZooKeeper(address, sessionTimeout, this);
 			Stat stat = zk.exists(root, false);
 			if (stat == null) {
-				// 创建根节点
-				zk.create(root, new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+				zk.create(root, new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);// 创建根节点
 				logger.debug("create root node {}", root);
 			}
-		} catch (IOException e) {
-			exception.add(e);
-		} catch (KeeperException e) {
-			exception.add(e);
-		} catch (InterruptedException e) {
-			exception.add(e);
+		} catch (Exception e) {
+			throw new LockException("could not connect to zookeeper!");
 		}
 	}
 
@@ -81,27 +70,14 @@ public class DistributedLock implements Lock, Watcher {
 	}
 
 	public void lock() {
-		if (exception.size() > 0) {
-			throw new LockException(exception.get(0));
-		}
-		try {
-			if (this.tryLock()) {
-				logger.debug("get lock {}", myZnode);
-				return;
-			} else {
-				waitForLock(waitNode, sessionTimeout);//等待锁
-			}
-		} catch (KeeperException e) {
-			throw new LockException(e);
-		} catch (InterruptedException e) {
-			throw new LockException(e);
+		boolean locked = tryLock(sessionTimeout, TimeUnit.MILLISECONDS);
+		if(!locked){
+			throw new LockException("get lock timeout");
 		}
 	}
 
 	public boolean tryLock() {
 		try {
-			if (lockName.contains(splitStr))
-				throw new LockException("lockName can not contains \\u000B");
 			//创建临时子节点
 			myZnode = zk.create(root + "/" + lockName + splitStr, new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
 			logger.debug("create node {}", myZnode);
@@ -132,25 +108,26 @@ public class DistributedLock implements Lock, Watcher {
 		return false;
 	}
 
-	public boolean tryLock(long time, TimeUnit unit) {
+	public boolean tryLock(long timeout, TimeUnit unit) {
 		try {
 			if (this.tryLock()) {
+				logger.debug("get lock {}", myZnode);
 				return true;
 			}
-			return waitForLock(waitNode, unit.toMillis(time));
+			return waitForLock(waitNode, timeout, unit);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return false;
 	}
 
-	private boolean waitForLock(String lower, long waitTime) throws InterruptedException, KeeperException {
+	private boolean waitForLock(String lower, long timeout, TimeUnit unit) throws InterruptedException, KeeperException {
 		Stat stat = zk.exists(root + "/" + lower, true);
 		//判断比自己小一个数的节点是否存在,如果不存在则无需等待锁,同时注册监听
 		if (stat != null) {
 			logger.debug("waiting for lock {}", root + "/" + lower);
 			this.latch = new CountDownLatch(1);
-			this.latch.await(waitTime, TimeUnit.MILLISECONDS);
+			this.latch.await(timeout, unit);
 			this.latch = null;
 		}
 		return true;
@@ -163,9 +140,9 @@ public class DistributedLock implements Lock, Watcher {
 			myZnode = null;
 			zk.close();
 		} catch (InterruptedException e) {
-			e.printStackTrace();
+			logger.error("interrupted", e);
 		} catch (KeeperException e) {
-			e.printStackTrace();
+			logger.error("keeper", e);
 		}
 	}
 
--- common-lang/src/test/java/concurrent/DistributedLockTest.java
+++ common-lang/src/test/java/concurrent/DistributedLockTest.java
@@ -1,13 +1,12 @@
 package concurrent;
 
 import com.jingcai.apps.common.lang.concurrent.DistributedLock;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.ExceptionDepthComparator;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * ConcurrentTask[] tasks = new ConcurrentTask[5];
@@ -20,131 +19,50 @@ import java.util.concurrent.atomic.AtomicInteger;
  * new ConcurrentTest(tasks);
  */
 public class DistributedLockTest {
-	private CountDownLatch startSignal = new CountDownLatch(1);//开始阀门
-	private CountDownLatch doneSignal = null;//结束阀门
-	private CopyOnWriteArrayList<Long> list = new CopyOnWriteArrayList<Long>();
-	private AtomicInteger err = new AtomicInteger();//原子递增
-	private ConcurrentTask[] tasks = null;
-
-	public DistributedLockTest(ConcurrentTask... tasks) {
-		this.tasks = tasks;
-		if (tasks == null) {
-			System.out.println("tasks can not null");
-			System.exit(1);
-		}
-		doneSignal = new CountDownLatch(tasks.length);
-		start();
-	}
-
-	private void start() {
-		//创建线程，并将所有线程等待在阀门处
-		createThread();
-		//打开阀门
-		startSignal.countDown();//递减锁存器的计数，如果计数到达零，则释放所有等待的线程
-		try {
-			doneSignal.await();//等待所有线程都执行完毕
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		//计算执行时间
-		getExeTime();
-	}
-
-	/**
-	 * 初始化所有线程，并在阀门处等待
-	 */
-	private void createThread() {
-		long len = doneSignal.getCount();
-		for (int i = 0; i < len; i++) {
-			final int j = i;
-			new Thread(new Runnable() {
-				public void run() {
-					try {
-						startSignal.await();//使当前线程在锁存器倒计数至零之前一直等待
-						long start = System.currentTimeMillis();
-						tasks[j].run();
-						long end = (System.currentTimeMillis() - start);
-						list.add(end);
-					} catch (Exception e) {
-						err.getAndIncrement();//相当于err++
-					}
-					doneSignal.countDown();
-				}
-			}).start();
-		}
-	}
-
-	/**
-	 * 计算平均响应时间
-	 */
-	private void getExeTime() {
-		int size = list.size();
-		List<Long> _list = new ArrayList<Long>(size);
-		_list.addAll(list);
-		Collections.sort(_list);
-		long min = _list.get(0);
-		long max = _list.get(size - 1);
-		long sum = 0L;
-		for (Long t : _list) {
-			sum += t;
-		}
-		long avg = sum / size;
-		System.out.println("min: " + min);
-		System.out.println("max: " + max);
-		System.out.println("avg: " + avg);
-		System.out.println("err: " + err.get());
-	}
-
-	public interface ConcurrentTask {
-		void run();
-	}
-
+	private static final Logger logger = LoggerFactory.getLogger(DistributedLockTest.class);
 	public static final String ZOOKEEPER_URI = "192.168.0.11:2181,192.168.0.18:2181,192.168.0.19:2181";
-	public static void main(String[] args) {
-		Runnable task1 = new Runnable(){
+	private CountDownLatch startSignal = new CountDownLatch(1);//开始阀门
+
+	@Test
+	public void test2(){
+		final DistributedLock lock = new DistributedLock(ZOOKEEPER_URI, "test2");
+		new Thread(new Runnable() {
 			public void run() {
-				DistributedLock lock = null;
-				try {
-					lock = new DistributedLock(ZOOKEEPER_URI, "test1");
-					//lock = new DistributedLock("127.0.0.1:2182","test2");
+				//try {startSignal.await();} catch (InterruptedException e) {e.printStackTrace();}
+				logger.debug("----------------------a1");
+
+				try{
 					lock.lock();
-					Thread.sleep(3000);
-					System.out.println("===Thread " + Thread.currentThread().getId() + " running");
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				finally {
-					if(lock != null)
-						lock.unlock();
+					logger.debug("----------------------a getlock");
+					try {Thread.sleep(3000);} catch (InterruptedException e) {}
+					//startSignal.countDown();
+				}catch (Exception e){
+					logger.error("a", e);
+				}finally {
+					logger.debug("----------------------a releaselock");
+					lock.unlock();
 				}
 			}
-		};
-		new Thread(task1).start();
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e1) {
-			e1.printStackTrace();
-		}
-		ConcurrentTask[] tasks = new ConcurrentTask[10];
-		for(int i=0;i<tasks.length;i++){
-			ConcurrentTask task3 = new DistributedLockTest.ConcurrentTask(){
-				public void run() {
-					DistributedLock lock = null;
-					try {
-						lock = new DistributedLock(ZOOKEEPER_URI,"test2");
-						lock.lock();
-						System.out.println("Thread " + Thread.currentThread().getId() + " running");
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-					finally {
-						lock.unlock();
-					}
+		}).start();
+		new Thread(new Runnable() {
+			public void run() {
+				logger.debug("----------------------b1");
+				try {Thread.sleep(1000);} catch (InterruptedException e) {}
 
+				try{
+					lock.lock();
+					logger.debug("----------------------b getlock");
+					//try {startSignal.await();} catch (InterruptedException e) {e.printStackTrace();}
+				}catch (Exception e){
+					logger.error("b", e);
+				}finally {
+					logger.debug("----------------------b releaselock");
+					lock.unlock();
 				}
-			};
-			tasks[i] = task3;
-		}
-		new DistributedLockTest(tasks);
+			}
+		}).start();
+		try {Thread.sleep(10000000);} catch (InterruptedException e) {}
 	}
+
+
 }
